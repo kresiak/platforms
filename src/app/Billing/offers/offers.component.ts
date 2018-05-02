374ccddbfbf9847f3e3598a42ccc9cc3c49f4b49
kresiak <kresiak1@gmail.com>
2018-05-02 14:57:30 +0200

    formStructure Add an Offer

--- src/app/Billing/offers/offers.component.ts
+++ src/app/Billing/offers/offers.component.ts
@@ -2,9 +2,9 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
 import { Observable } from 'rxjs/Rx'
 import { DataStore } from 'gg-basic-data-services'
 import { PlatformService } from '../../Services/platform.service'
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import {utilsComparators as comparatorsUtils} from 'gg-search-handle-data'
 import * as moment from "moment"
+import { FormItemStructure, FormItemType} from 'gg-ui'
 
 @Component(
     {
@@ -13,20 +13,20 @@ import * as moment from "moment"
     }
 )
 export class PlatformOffersComponent implements OnInit {
-    constructor (private formBuilder: FormBuilder, private dataStore: DataStore, private platformService: PlatformService) {
+    constructor ( private dataStore: DataStore, private platformService: PlatformService) {
     }
 
-public offerForm: FormGroup
 public offersList: any
 public offersObservable
 public isPageRunning: boolean = true
 public clientsListObservable
 public clientId: string
+public formStructure: FormItemStructure[]= []
 
     ngOnInit(): void {
-        this.offerForm = this.formBuilder.group({
-            description: ['', [Validators.required, Validators.minLength(3)]]
-        })
+
+        this.formStructure.push(new FormItemStructure('description', 'PLATFORM.OFFER.LABEL.DESCRIPTION', FormItemType.InputText, {isRequired: true, minimalLength: 3}))
+        this.formStructure.push(new FormItemStructure('clientId', 'PLATFORM.OFFER.LABEL.CHOOSE A CLIENT', FormItemType.FlexiList, {isRequired: true, selectableData: this.clientsListObservable }))
 
         this.offersObservable=this.platformService.getAnnotatedOffers()
 
@@ -47,26 +47,22 @@ public clientId: string
         this.clientId = clientId
     }
 
-    save(formValue, isValid) {
+    formSaved(data) {
         var datPrefix= moment().format('YYYYMMDD') + '-GEN-'
         var offerNo= this.offersList.filter(o => o.data.prefix===datPrefix).map(o => +o.data.offerNo).sort((a,b) => b-a)[0] || 0
 
-        this.dataStore.addData('platform.offers', {
-            prefix: datPrefix,
-            offerNo: offerNo + 1,
-            version: 0,
-            description: formValue.description,
-            clientId: this.clientId
-        }).subscribe(res =>
-        {
-            this.reset()
-        })
-    }
+        var offers: any = {}
+            offers.prefix = data.datPrefix,
+            offers.offerNo = data.offerNo + 1,
+            data.version = 0,
+            offers.description = data.description,
+            offers.clientId = this.clientId
 
-    reset()
-    {
-        this.offerForm.reset()
-    }
+        this.dataStore.addData('platform.offers', offers ).first().subscribe(res =>
+            {
+                data.setSuccess('OK')            
+            });
+        }
 
     ngOnDestroy(): void {
         this.isPageRunning = false
